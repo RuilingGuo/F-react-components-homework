e99ad2456cde7ecc97c8102cc6e8010ad81e8c16
RuilingGuo <ruiling.guo1@thoughtworks.com>
2020-09-22 21:36:20 +0800

    [ruiling] feat: add auto response

--- src/Chat/Chat.js
+++ src/Chat/Chat.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './Chat.scss';
 import ChatHeader from './ChatHeader/ChatHeader';
 import ChatBox from './ChatBox/ChatBox';
@@ -7,42 +7,59 @@ import shopData from '../data/shop.json';
 import answersData from '../data/answers.json';
 
 class Chat extends Component {
-  constructor(props, context) {
-    super(props, context);
-    this.state = {
-      shop: {},
-      messages: [],
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            shop: {},
+            messages: [],
+        };
+    }
+
+    componentDidMount() {
+        const defaultMessage = answersData.find((answer) => answer.tags.includes('DEFAULT'));
+        const messages = this.state.messages.concat(defaultMessage);
+
+        setTimeout(() => {
+            this.setState({
+                shop: shopData,
+                messages,
+            });
+        }, 1000);
+    }
+
+    addNewMessage = (newMessage) => {
+        this.setState((preState) => ({
+            messages: [...preState.messages, newMessage],
+        }));
     };
-  }
 
-  componentDidMount() {
-    const defaultMessage = answersData.find((answer) => answer.tags.includes('DEFAULT'));
-    const messages = this.state.messages.concat(defaultMessage);
+    autoResponseMessage = async (text) => {
+        const tags = answersData.flatMap(answer => answer.tags);
+        tags.forEach(tag => {
+            if (text.includes(tag)) {
+                const responseMessage = answersData.find(answer > answer.tags.includes(tag));
+                this.addNewMessage(responseMessage);
+            }
+        });
+    };
 
-    setTimeout(() => {
-      this.setState({
-        shop: shopData,
-        messages,
-      });
-    }, 1000);
-  }
+    updateMessages = (newMessage) => {
+        this.addNewMessage(newMessage);
+        this.autoResponseMessage(newMessage.text)
+            .catch(err => console.log(err));
+    };
 
-  inputNewMessage = (newMessage) => {
-    this.setState((preState) => ({
-      messages: [...preState.messages, newMessage],
-    }));
-  };
 
-  render() {
-    const { shop, messages } = this.state;
-    return (
-      <main className="Chat">
-        <ChatHeader shop={shop} />
-        <ChatBox messages={messages} />
-        <ChatInput inputNewMessage = {this.inputNewMessage}/>
-      </main>
-    );
-  }
+    render() {
+        const {shop, messages} = this.state;
+        return (
+            <main className="Chat">
+                <ChatHeader shop={shop}/>
+                <ChatBox messages={messages}/>
+                <ChatInput inputNewMessage={this.updateMessages}/>
+            </main>
+        );
+    }
 }
 
 export default Chat;
